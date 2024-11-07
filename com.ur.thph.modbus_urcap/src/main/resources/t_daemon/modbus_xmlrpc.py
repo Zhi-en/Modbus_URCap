5b78dc13c995058623f20193686c2723a77222e1
Zhi-en Tan <tan.zhien@dyson.com>
2024-11-07 14:37:04 +0800

    streamline struct format

--- com.ur.thph.modbus_urcap/src/main/resources/t_daemon/modbus_xmlrpc.py
+++ com.ur.thph.modbus_urcap/src/main/resources/t_daemon/modbus_xmlrpc.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-# Changelog
-# 2024-10-10: Wilfrid: Added more functions. Renamed init_modbus_communications to init_tool_modbus
-# 2024-10-17: Wilfrid: Extended all functions to error handling versions which return struct.
-# 2024-10-21: Zhi-en: Add error handling and number of bits (16, 32 or 64) to setting options
 
 import socket
 import struct
@@ -35,6 +31,42 @@ error_handling = True
 def isReachable():
   return True
 
+def init_tool_modbus_64bit(slaveaddress):
+  init_tool_modbus(slaveaddress)
+  global num_of_registers
+  num_of_registers = 4
+  return True
+
+def init_tool_modbus_32bit(slaveaddress):
+  init_tool_modbus(slaveaddress)
+  global num_of_registers
+  num_of_registers = 2
+  return True
+
+def init_tool_modbus_16bit(slaveaddress):
+  init_tool_modbus(slaveaddress)
+  global num_of_registers
+  num_of_registers = 1
+  return True
+
+def init_tool_modbus_no_error_handling_64bit(slaveaddress):
+  init_tool_modbus_64bit(slaveaddress)
+  global error_handling
+  error_handling = False
+  return True
+
+def init_tool_modbus_no_error_handling_32bit(slaveaddress):
+  init_tool_modbus_32bit(slaveaddress)
+  global error_handling
+  error_handling = False
+  return True
+
+def init_tool_modbus_no_error_handling_16bit(slaveaddress):
+  init_tool_modbus_16bit(slaveaddress)
+  global error_handling
+  error_handling = False
+  return True
+
 def init_tool_modbus(slaveaddress):
   global instrument
   #instrument = modbus.Instrument('/dev/ttyUSB0',slaveaddress)
@@ -53,14 +85,7 @@ def init_tool_modbus(slaveaddress):
   instrument.serial.timeout = timeout
   global error_handling
   error_handling = True
-  return True
 
-def init_tool_modbus_no_error_handling(slaveaddress):
-  ''' only 1 of this or init_tool_modbus should be called to initiate the modbus URCAP '''
-  init_tool_modbus(slaveaddress)
-  global error_handling
-  error_handling = False
-  return True
 
 ''' Functions for modifying default settings '''
 def tool_modbus_set_baudrate(baudrate):
@@ -101,14 +126,6 @@ def tool_modbus_set_timeout(timeout):
   instrument.serial.timeout = timeout
   return True
 
-def tool_modbus_set_register_chaining(register_chaining):
-  if register_chaining in (1,2,4):
-    global num_of_registers
-    num_of_registers = register_chaining
-    return True
-  else:
-    return False
-
 
 ''' Function for checking if connected in no error handling mode '''
 def tool_modbus_check_connection(register_type, register):
@@ -141,11 +158,10 @@ def tool_modbus_check_connection(register_type, register):
 ''' Functions for bit communication '''
 def tool_modbus_write_coil(register_address, data):
   '''Function to write single BOOL to slave'''
-  result = {"error": "", "error_flag": False, "value": False}
+  result = {"value": data, "error": "", "error_flag": False}
   try:
     data = bool(data)
     instrument.write_bit(register_address, data, 5)
-    result["value"] = True
   except Exception as e:
     Logger.error("data: %s", data)
     Logger.error("Error in modbus write method", exc_info=True)
@@ -156,11 +172,11 @@ def tool_modbus_write_coil(register_address, data):
   elif result["error_flag"]:
     return result["error"]
   else:
-    return result["value"]
+    return True
 
 def tool_modbus_read_discrete(register_address):
   '''Function to read single BOOL from slave'''
-  result = {"error": "", "error_flag": False, "value": False}
+  result = {"value": False, "error": "", "error_flag": False}
   try:
     value = instrument.read_bit(register_address)
     result["value"] = bool(value)
@@ -177,7 +193,7 @@ def tool_modbus_read_discrete(register_address):
 
 def tool_modbus_read_coil(register_address):
   '''Function to read back BOOL from master'''
-  result = {"error": "", "error_flag": False, "value": False}
+  result = {"value": False, "error": "", "error_flag": False}
   try:
     value = instrument.read_bit(register_address, functioncode=1)
     result["value"] = bool(value)
@@ -208,7 +224,7 @@ def unsigned_to_signed(num_bits, value):
 ''' Funcionts for 16/32/64 bit communication '''
 def tool_modbus_write_holding_int(register_address, data):
   '''Function to write INT16/INT32/INT64 to slave using 1/2/4 registers. Maps register_address to multiples of 1/2/4 to ensure no overlap.'''
-  result = {"error": "", "error_flag": False, "value": False}
+  result = {"value": data, "error": "", "error_flag": False}
   try:
     data = int(data)
     data = signed_to_unsigned(num_of_registers*16, data)    # most significant bit denotes sign
@@ -216,7 +232,6 @@ def tool_modbus_write_holding_int(register_address, data):
     for i in range(num_of_registers-1, -1, -1):    # start from highest 16 bits to lowest 16 bits
       val.append((data >> 16*i) & 0xFFFF)
     instrument.write_registers(register_address*num_of_registers,val)
-    result["value"] = True
   except Exception as e:
     Logger.error("data: %s", data)
     for i in range(len(val)):
@@ -229,18 +244,17 @@ def tool_modbus_write_holding_int(register_address, data):
   elif result["error_flag"]:
     return result["error"]
   else:
-    return result["value"]
+    return True
 
 def tool_modbus_write_holding_float(register_address, data):
   '''Function to write FLOAT32/FLOAT64 to slave using 2/4 registers. Maps register_address to multiples of 2/4 to ensure no overlap. Will return error if num_of_registers = 1 '''
-  result = {"error": "", "error_flag": False, "value": False}
+  result = {"value": data, "error": "", "error_flag": False}
   if num_of_registers <2:
     result["error"] =  "16-bit does not support float types"
     result["error_flag"] = True
   else:
     try:
       instrument.write_float(register_address*num_of_registers, data, number_of_registers=num_of_registers)
-      result["value"] = True
     except Exception as e:
       Logger.error("data: %s", data)
       Logger.error("Error in modbus write method", exc_info=True)
@@ -251,11 +265,11 @@ def tool_modbus_write_holding_float(register_address, data):
   elif result["error_flag"]:
     return result["error"]
   else:
-    return result["value"]
+    True
 
 def tool_modbus_read_input_int(register_address):
   '''Function to read INT16/INT32/INT64 from slave using 1/2/4 registers. Maps register_address to multiples of 1/2/4 to ensure no overlap.'''
-  result = {"error": "", "error_flag": False, "value": 0}
+  result = {"value": 0, "error": "", "error_flag": False}
   try:
     val = instrument.read_registers(register_address*num_of_registers, num_of_registers, functioncode=4)
     val_comb = val[0]
@@ -275,7 +289,7 @@ def tool_modbus_read_input_int(register_address):
 
 def tool_modbus_read_input_float(register_address):
   '''Function to read FLOAT32/FLOAT64 from slave using 2/4 registers. Maps register_address to multiples of 2/4 to ensure no overlap. Will return error if num_of_registers = 1 '''
-  result = {"error": "", "error_flag": False, "value": 0.0}
+  result = {"value": 0.0, "error": "", "error_flag": False}
   if num_of_registers <2:
     result["error"] =  "16-bit does not support float types"
     result["error_flag"] = True
@@ -294,7 +308,7 @@ def tool_modbus_read_input_float(register_address):
 
 def tool_modbus_read_holding_int(register_address):
   '''Function to read back INT16/INT32/INT64 from master using 1/2/4 registers. Maps register_address to multiples of 1/2/4 to ensure no overlap.'''
-  result = {"error": "", "error_flag": False, "value": 0}
+  result = {"value": 0, "error": "", "error_flag": False}
   try:
     val = instrument.read_registers(register_address*num_of_registers, num_of_registers, functioncode=3)
     val_comb = val[0]
@@ -314,7 +328,7 @@ def tool_modbus_read_holding_int(register_address):
 
 def tool_modbus_read_holding_float(register_address):
   '''Function to read back FLOAT32/FLOAT64 from master using 2/4 registers. Maps register_address to multiples of 2/4 to ensure no overlap. Will return error if num_of_registers = 1 '''
-  result = {"error": "", "error_flag": False, "value": 0.0}
+  result = {"value": 0.0, "error": "", "error_flag": False}
   if num_of_registers <2:
     result["error"] =  "16-bit does not support float types"
     result["error_flag"] = True
@@ -346,7 +360,6 @@ def tool_modbus_write_holding(register_address, data):
 
 def tool_modbus_read_input(register_address):
   '''Function to read INT-16 data from slave using 1 register'''
-  global value
   try:
     value = int(instrument.read_register(register_address, 0, 4, True))
   except Exception as e:
@@ -471,14 +484,17 @@ server.RequestHandlerClass.protocol_version = "HTTP/1.1"
 print("Listening on port 40408...")
 
 server.register_function(isReachable,"isReachable")
-server.register_function(init_tool_modbus,"init_tool_modbus")
-server.register_function(init_tool_modbus_no_error_handling,"init_tool_modbus_no_error_handling")
+server.register_function(init_tool_modbus_64bit,"init_tool_modbus_64bit")
+server.register_function(init_tool_modbus_32bit,"init_tool_modbus_32bit")
+server.register_function(init_tool_modbus_16bit,"init_tool_modbus_16bit")
+server.register_function(init_tool_modbus_no_error_handling_64bit,"init_tool_modbus_no_error_handling_64bit")
+server.register_function(init_tool_modbus_no_error_handling_32bit,"init_tool_modbus_no_error_handling_32bit")
+server.register_function(init_tool_modbus_no_error_handling_16bit,"init_tool_modbus_no_error_handling_16bit")
 server.register_function(tool_modbus_set_baudrate,"tool_modbus_set_baudrate")
 server.register_function(tool_modbus_set_bytesize,"tool_modbus_set_bytesize")
 server.register_function(tool_modbus_set_parity,"tool_modbus_set_parity")
 server.register_function(tool_modbus_set_stopbit,"tool_modbus_set_stopbit")
 server.register_function(tool_modbus_set_timeout,"tool_modbus_set_timeout")
-server.register_function(tool_modbus_set_register_chaining,"tool_modbus_set_register_chaining")
 server.register_function(tool_modbus_check_connection,"tool_modbus_check_connection")
 server.register_function(tool_modbus_write_coil,"tool_modbus_write_coil")
 server.register_function(tool_modbus_read_discrete,"tool_modbus_read_discrete")
